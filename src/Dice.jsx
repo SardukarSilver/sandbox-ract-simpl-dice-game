dca064add14b9901286ee66a778c658e4f7a3e47
Nikita Chotiy <nikita.chotiy@eosda.com>
2020-02-28 12:04:00 +0200

    init commit

---
+++ src/Dice.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Dice.css'
+
+class Dice extends Component {
+    render() {
+        return <i className={`Dice fas fa-dice-${this.props.face} ${this.props.rolling && 'shaking'}`} />
+    }
+}
+
+export default Dice
