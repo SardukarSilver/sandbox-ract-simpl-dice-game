1986f859bd40c3bc8b1342bc9e4cc361df4902f1
Nikita Chotiy <nikita.chotiy@eosda.com>
2020-02-28 12:16:25 +0200

    Updates

---
+++ precache-manifest.d096e9790d82a6d37bdb40ef02d2aea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14ecde1f70715ce37f79aafbd6ca483",
+    "url": "/sandbox-ract-simpl-dice-game/index.html"
+  },
+  {
+    "revision": "4887c7aa29f9431d4030",
+    "url": "/sandbox-ract-simpl-dice-game/static/css/main.3d670df6.chunk.css"
+  },
+  {
+    "revision": "42456883e8788966a764",
+    "url": "/sandbox-ract-simpl-dice-game/static/js/2.f17b18d2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sandbox-ract-simpl-dice-game/static/js/2.f17b18d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4887c7aa29f9431d4030",
+    "url": "/sandbox-ract-simpl-dice-game/static/js/main.3d640149.chunk.js"
+  },
+  {
+    "revision": "775d4a3d70efde7525d2",
+    "url": "/sandbox-ract-simpl-dice-game/static/js/runtime-main.4e4358fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f04055aed2e325e3ac4543efc87954f3.js"
+  "/sandbox-ract-simpl-dice-game/precache-manifest.d096e9790d82a6d37bdb40ef02d2aea6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sandbox-ract-simpl-dice-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
